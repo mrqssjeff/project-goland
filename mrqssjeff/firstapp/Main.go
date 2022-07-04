0c022c521d3f52eb324d4f99bb0fb87983a77b73
Jefferson Marques <98566026+mrqssjeff@users.noreply.github.com>
2022-07-04 19:32:30 -0300

    govariables

--- mrqssjeff/firstapp/Main.go
+++ mrqssjeff/firstapp/Main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"	
+	"fmt"
+	"strconv"
 )
 
-var i float32 = 42
+var (
+	anime string = "One Piece"
+	i float32 = 42;
+)
 
-func main(){	
-	fmt.Printf("%v, %T", i ,i)
+/**
+declared variables must be used;
+lower case variables are scoped to the package;
+upper case variables then its globally visible;
+to convert to string you need to use the strconv package;
+**/
+func main(){
+	var luke int = 89
+	k := 99
+	var luka float32 = float32(luke)
+	var text string = strconv.Itoa(k)
+	fmt.Printf("%v, %T", i ,luka)
+	fmt.Println(k)
+	fmt.Println(anime)
+	fmt.Println(luke)
+	fmt.Println(text)
+
 }
